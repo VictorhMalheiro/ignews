82e12f459bfe4ca4120a32f6099a5874baea5f95
Victor Hugo Malheiro <victorhugobarretos@gmail.com>
2021-03-26 01:09:43 -0300

    Style and Config

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
